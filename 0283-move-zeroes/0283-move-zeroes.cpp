4f674e87825a2a8b9a96950fa37fae272d990a00
samarth-bhatt <123242629+samarth-bhatt@users.noreply.github.com>
2024-08-15 00:27:32 +0530

    Time: 17 ms (45.77%), Space: 21.9 MB (48.39%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,14 +3,15 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return;
-        int cnt_zero = 0;
+        int nonZeroIdx = 0;
         for(int i=0; i<n; i++){
-            if(nums[i] == 0){
-                int j = i+1;
-                while(j<n && nums[j] == 0) j++;
-                if(j == n) return;
-                swap(nums[i], nums[j]);
+            if(nums[i] != 0){
+                nums[nonZeroIdx] = nums[i];
+                nonZeroIdx++;
             }
         }
+        int i=0;
+        int zeroCnt = n - nonZeroIdx;
+        while(zeroCnt--){ nums[n - 1 - i] = 0; i++;}
     }
 };
